5bad2d31368594110331c65a71a5acf2466f2d21
keyla <keysugar2009@gmail.com>
2023-08-13 17:12:53 -0600

    creación clase para leer archivos

--- ProyectoProgra2/LeerArchivo.cs
+++ ProyectoProgra2/LeerArchivo.cs
@@ -6,6 +6,33 @@ namespace ProyectoProgra2
 {
     public class LeerArchivo
     {
+        private string _ruta;
+        private List<string> empleados = new List<string>();
 
+        public LeerArchivo(string rutaNueva)
+        {
+            _ruta = rutaNueva;
+        }
+
+        public void LeerEmpleado()
+        {
+            using (StreamReader lector = new StreamReader(_ruta))
+            {
+                while (lector.EndOfStream != true)
+                {
+                    Console.WriteLine(lector.ReadLine());
+                }
+                lector.Close();
+            }
+        }
+
+        public void AgregarEmpleado(string empleado)
+        {
+            using (StreamWriter lector = new StreamWriter(_ruta, true))
+            {
+                lector.WriteLine(empleado);
+                lector.Close();
+            }
+        }
     }
 }
