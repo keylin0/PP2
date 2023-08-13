7f85024598c97c8eaf13aa82e058ac08f95ab642
keyla <keysugar2009@gmail.com>
2023-08-13 17:05:24 -0600

    creación del proyecto

---
+++ ProyectoProgra2/LeerArchivo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ProyectoProgra2
+{
+    public class LeerArchivo
+    {
+
+    }
+}
---
+++ ProyectoProgra2/Menu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace ProyectoProgra2
+{
+    internal class Menu
+    {
+        private static void Main(string[] args)
+        {
+
+        }
+    }
+}
