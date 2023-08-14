1ebbb86972a466ffe87dfe246dcb8f6b5ae6976b
keyla <keysugar2009@gmail.com>
2023-08-14 14:52:02 -0600

    Se creo una clase con el menu y una clase para la lectura de archivos, ademas se creo el archivo txt empleados

--- ProyectoProgra2/LeerArchivo.cs
+++ ProyectoProgra2/LeerArchivo.cs
@@ -7,7 +7,9 @@ namespace ProyectoProgra2
     public class LeerArchivo
     {
         private string _ruta;
-        private List<string> empleados = new List<string>();
+        private List<string> nombres = new List<string>();
+        private List<string> cedulas = new List<string>();
+        private List<string> correos = new List<string>();
 
         public LeerArchivo(string rutaNueva)
         {
@@ -31,6 +33,32 @@ namespace ProyectoProgra2
             using (StreamWriter lector = new StreamWriter(_ruta, true))
             {
                 lector.WriteLine(empleado);
+                lector.Close();
+            }
+        }
+
+        public void crearArchivo(string datos, string ruta) {
+            using (StreamWriter lector = new StreamWriter(ruta, false))
+            {
+                lector.WriteLine(datos);
+                lector.Close();
+            }
+        }
+
+        public void CargarDatos(string ruta) {
+            using (StreamReader lector = new StreamReader(_ruta))
+            {
+                while (lector.EndOfStream != true)
+                {
+                    string str = lector.ReadLine();
+                    string[] lista = str.Split(' ');
+                    cedulas.Add(lista[0]);
+                    nombres.Add(lista[1]);
+                }
+                foreach (var emple in cedulas)
+                {
+                    Console.WriteLine(emple);
+                }
                 lector.Close();
             }
         }
--- ProyectoProgra2/Menu.cs
+++ ProyectoProgra2/Menu.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 
 namespace ProyectoProgra2
 {
@@ -7,7 +6,67 @@ namespace ProyectoProgra2
     {
         private static void Main(string[] args)
         {
-
+            string path = @"C:\Proyecto2\ProyectoProgra2\archivos\Empleado.txt";
+            LeerArchivo archivo = new LeerArchivo(path);
+            string menuPrincipal = " ***** Menu ***** \n" +
+                            "1 - Ver salarios \n" +
+                            "2 - Agregar Empleados \n" +
+                            "3 - Salir \n" +
+                            "Elija una opcion: ";
+            string menuSalarios = "***** Salarios de los empleados *****\n" +
+                "**** Imprimir o salir **** \n" +
+                "1. Imprimir \n" +
+                "2. Salir \n" +
+                "Elija una opcion: ";
+            int opcionMenu = 0;
+            bool validaMenu = false;
+            bool validaOpcion = true;
+            int opcionCliente;
+            while (opcionMenu != 3)
+            {
+                Console.WriteLine(menuPrincipal);
+                validaMenu = int.TryParse(Console.ReadLine(), out opcionMenu);
+                if (validaMenu == true)
+                {
+                    if (opcionMenu == 1)
+                    {
+                        Console.Clear();
+                        Console.WriteLine(menuSalarios);
+                        validaMenu = int.TryParse(Console.ReadLine(), out opcionCliente);
+                        do
+                        {
+                            if (opcionCliente == 1)
+                            {
+                                Console.Clear();
+                            }
+                            else if (opcionCliente == 2)
+                            {
+                                Console.Clear();
+                                validaOpcion = false;
+                            }
+                            else
+                            {
+                                Console.WriteLine("opcion invalida, intente nuevamente");
+                                validaMenu = int.TryParse(Console.ReadLine(), out opcionCliente);
+                            }
+                        } while (validaOpcion);
+                    }
+                    else if (opcionMenu == 2)
+                    {
+                        Console.Clear();
+                        archivo.CargarDatos(path);
+                    }
+                    else if (opcionMenu == 3)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("El valor no es parte del menu");
+                }
+                validaOpcion = true;
+            }
         }
     }
 }
